ce93c27fdced7b6d013e5c5bd656de89e6146511
lijiamei <1243001219@qq.com>
2021-04-01 17:09:10 +0800

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = (grunt) => {
+  grunt.initConfig({
+  less: {
+    compiled:{
+      files: {
+        'dist/compiled.css': 'css/layout.less'
+      }
+    }
+  }
+  
+
+  });
+ 
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.registerTask('default', ['less:compiled']);
+};
